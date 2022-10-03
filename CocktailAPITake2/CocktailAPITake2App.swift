9228522604b5706bc4e071f248d3436daa77e7db
kimccarthy <kimccarthy51@gmail.com>
2022-10-03 12:45:28 -0600

    Initial Commit

---
+++ CocktailAPITake2/CocktailAPITake2App.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailAPITake2App.swift
+//  CocktailAPITake2
+//
+//  Created by Kate McCarthy on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CocktailAPITake2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
